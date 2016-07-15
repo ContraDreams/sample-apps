20cdbff7b707fb95a4515d0d80b40857093d40c2
zeryx <james@algorithmia.com>
2016-07-15 09:42:45 -0700

    converted it to prod version.

--- JavaScript/colorization-demo/public/js/main.js
+++ JavaScript/colorization-demo/public/js/main.js
@@ -4,7 +4,7 @@
 // paris https://images.unsplash.com/33/YOfYx7zhTvYBGYs6g83s_IMG_8643.jpg
 
 window.Algorithmia = window.Algorithmia || {};
-Algorithmia.api_key = "sim6fN4dbIEi+ORCaElcoOteGB51"
+Algorithmia.api_key = "simeyUbLXQ/R8Qga/3ZCRGcr2oR1"
 var numTasks = 0;
 
 function callAlgorithm() {
@@ -36,34 +36,34 @@ document.getElementById('compareLink').addEventListener('click', function() {
 
 
 function colorify(img) {
-  Algorithmia.client(Algorithmia.api_key, "https://api-region-6.algorithmia.com/v1/web/algo")
-    .algo("algo://algorithmiahq/ColorizationDemo/0.1.6")
-    .pipe(img)
-    .then(function(output) {
-      if(output.error) {
-        // Error Handling
-        var statusLabel = document.getElementById("status-label")
-        statusLabel.innerHTML = '<div class="alert alert-danger" role="alert">Uh Oh! Something went wrong: ' + output.error.message + ' </div>';
-        taskError();
-      } else {
-        console.log("got output", output.result);
+  Algorithmia.client(Algorithmia.api_key)
+      .algo("algo://algorithmiahq/ColorizationDemo/1.1.0")
+      .pipe(img)
+      .then(function (output) {
+        if (output.error) {
+          // Error Handling
+          var statusLabel = document.getElementById("status-label")
+          statusLabel.innerHTML = '<div class="alert alert-danger" role="alert">Uh Oh! Something went wrong: ' + output.error.message + ' </div>';
+          taskError();
+        } else {
+          console.log("got output", output.result);
 
-        // Decode base64 imgs
-        var imgOriginal = "data:image/png;base64," + output.result[0];
-        var imgColorized = "data:image/png;base64," + output.result[1];
+          // Decode base64 imgs
+          var imgOriginal = "data:image/png;base64," + output.result[0];
+          var imgColorized = "data:image/png;base64," + output.result[1];
 
-        // Show the download link if API also returned the URL
-        if(output.result.length > 2) {
+          // Show the download link if API also returned the URL
+          if (output.result.length > 2) {
             document.getElementById("downloadLinks").classList.remove("hidden");
             document.getElementById("resultLink").href = output.result[2];
-        } else {
+          } else {
             document.getElementById("downloadLinks").classList.add("hidden");
             document.getElementById("resultLink").href = '#';
-        }
+          }
 
-        getMeta(imgOriginal, imgColorized);
-      }
-    });
+          getMeta(imgOriginal, imgColorized);
+        }
+      });
 }
 
 function getMeta(original,colorized){
